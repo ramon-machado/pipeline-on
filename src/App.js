d4c437506eaebac40fcac7de7dd1021f00ada14c
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2023-06-21 19:05:38 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={mba} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to FIAP 21AGL"</h1>
+          <h1 className="App-title">Welcome to 2023 - MBA AS_06"</h1>
         </header>
         <p className="App-intro">
           Minha Feature Devops
