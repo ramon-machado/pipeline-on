e8823cba75f513c3188d9b92285750e021a41ac0
Pamela macedo <114959652+plomacedo@users.noreply.github.com>
2023-08-07 21:29:43 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
-import mba from './OctoCat.jpg';
+import mba from './download.png';
 import './App.css';
 
 class App extends Component {
