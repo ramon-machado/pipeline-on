9235465c99fdad81eb68b5147c758a1712d864fb
Ramon Rodrigo Machado <rmorm5@gmail.com>
2023-10-03 21:26:40 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={mba} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to 2023 - SCJR"</h1>
+          <h1 className="App-title">Welcome to 2023 - 3DVPR"</h1>
         </header>
       </div>
     );
